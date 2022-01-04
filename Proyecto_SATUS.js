70b2754c373f74c9866b888a33f51397ddd69216
Luis Bringas <acevedo.bringas.luis.esteban@gmail.com>
2022-01-03 19:10:35 -0600

    Smooth Scroll

--- Proyecto_SATUS.js
+++ Proyecto_SATUS.js
@@ -17,10 +17,10 @@ MenuMovil.addEventListener('click', () => {
 //Galeria - Galeria Principal
 const ImagenesContenedor = document.querySelectorAll(".Contenedor_Imagenes_GaleriaPrincipal img");
 
-const nextImageDelay = 5000;
+const Delay = 5000;
 let ContadorImagen = 0; // setting a variable to keep track of the current image (slide)
 ImagenesContenedor[ContadorImagen].style.opacity = 1;
-setInterval(ImagenSiguiente_GP, nextImageDelay);
+setInterval(ImagenSiguiente_GP, Delay);
 
 function ImagenSiguiente_GP() {
     ImagenesContenedor[ContadorImagen].style.opacity = 0;
